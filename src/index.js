d4f3bf101f6ed37f8f020608d3e928896c0f5ca1
Gulay Kulekci <gulay.kulekci1991@gmail.com>
2021-03-31 19:30:30 +0300

    added-redux-store-configuration

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from "react-redux"
+import configureStore from "./redux/reducers/configureStore";
+
+const store = configureStore();
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
