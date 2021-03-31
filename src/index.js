a7a6b3f11715c12f713816eee6c4f458fde93def
Gulay Kulekci <gulay.kulekci1991@gmail.com>
2021-03-31 19:56:35 +0300

    connect-component-with-reducer-and-take-state-data

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/root/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Provider} from "react-redux"
-import configureStore from "./redux/reducers/configureStore";
+import configureStore from "./redux/reducers/configureStore"
+
 
 const store = configureStore();
-ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
